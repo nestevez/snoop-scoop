1c2b0b820aa43ebaacd43f08ad1291e2a291809e
Patrick <kaorti@gmail.com>
2017-12-11 16:51:42 -0800

    basic framework, forst stab at nlp api call code

---
+++ snoop_scoop/snoop_scoop/ApiCaller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.WebUtilities;
+
+
+namespace ApiCaller
+{
+    public class ApiCaller
+    {
+        public static async Task PolicyParsingRequest(string ConnectionString, string PolicyText)
+        {
+            using(var client = new HttpClient())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ConnectionString);
+
+                    var QueryParameters = new Dictionary();
+                    var uri = "https://westus.api.cognitive.microsoft.com/linguistics/v1.0/analyze?";
+                    var BodyText = new
+                    {
+                        language = "en",
+                        analyzerIds = "08ea174b-bfdb-4e64-987e-602f85da7f72",
+                        text = PolicyText
+                    };
+                    Json(BodyText)
+                    Console.WriteLine("Body:", BodyText);
+                    byte[] EncodedText = Encoding.UTF8.GetBytes(BodyText.ToString());
+                    var content = new ByteArrayContent(EncodedText);
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    HttpResponseMessage response = await client.PostAsync(uri, content);
+                    response.EnsureSuccessStatusCode();
+                     
+                }
+                catch (HttpRequestException exception)
+                {
+                    Console.WriteLine($"Request exception: {exception.Message}");
+                }
+            }
+        }
+    }
+}
